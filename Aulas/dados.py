8fd4b5854f879e294f42a471efc6e72f2f74bc0e
Luiz Eduardo <oliveiraluiz.com@hotmail.com>
2021-05-18 18:38:21 -0300

    Finalizada a aula 3

--- Aulas/dados.py
+++ Aulas/dados.py
@@ -5,13 +5,28 @@ def carregar_acessos():
     X = []
     Y = []
 
-    arquivo = open('../acesso.csv', 'r')
+    arquivo = open('../Arquivos/acesso.csv', 'r')
     leitor = csv.reader(arquivo)
 
     next(leitor)
 
     for home, como_funciona, contato, comprou in leitor:
         X.append([int(home), int(como_funciona), int(contato)])
+        Y.append(int(comprou))
+
+    return X, Y
+
+def carregar_buscas():
+    X = []
+    Y = []
+
+    arquivo = open('../Arquivos/buscas.csv', 'r')
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+
+    for home, busca, logado, comprou in leitor:
+        X.append([int(home), busca, int(logado)])
         Y.append(int(comprou))
 
     return X, Y
