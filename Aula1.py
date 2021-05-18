9a2ec6a5e7af881259c3caf83cbe14acf5f4d82f
Luiz Eduardo <oliveiraluiz.com@hotmail.com>
2021-05-18 16:24:04 -0300

    Aula 1 utilizando sklearn

---
+++ Aula1.py
@@ -0,0 +1,20 @@
+from sklearn.naive_bayes import *
+
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+teste = [misterioso1, misterioso2]
+
+modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
+print(modelo.predict(teste))
